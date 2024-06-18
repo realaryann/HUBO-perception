b53179082f18174e2e8fb70e7e1c180f70c0688a
MacksCohn <maxcohn@live.com>
2024-06-18 15:33:53 -0400

    Simple working gazebo project and empty base code

---
+++ pointcloud_practice/src/pointcloud_practice.cpp
@@ -0,0 +1,7 @@
+#include "rclcpp/rclcpp.hpp"
+// Run the following to get point cloud information
+// gazebo /opt/ros/humble/share/gazebo_plugins/worlds/gazebo_ros_depth_camera_demo.world
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
