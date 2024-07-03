34ce29c4a2dc901783349dafb237a82ca9e5e554
MacksCohn <maxcohn@live.com>
2024-07-03 11:54:46 -0400

    Retrained, need more images for lighting and background variations

--- image_parsing/image_parsing/datasets/train.py
+++ image_parsing/image_parsing/datasets/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8s.pt')
 
-model.train(data='./data.yml', batch=8, imgsz=640, epochs=100, workers=1)
+model.train(data='./data.yml', batch=8, imgsz=640, epochs=300, workers=1)
