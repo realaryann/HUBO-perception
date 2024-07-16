39b27cb06e82cd5714e34ed30b8119e2dc6e933b
MacksCohn <maxcohn@live.com>
2024-07-16 15:27:25 -0400

    Added verbose to training

--- image_parsing/image_parsing/datasets/train.py
+++ image_parsing/image_parsing/datasets/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8m.pt')
 
-model.train(data='./data.yml', batch=12, imgsz=640, epochs=400, workers=1, device=(0,1,2))
+model.train(data='./data.yml', batch=12, imgsz=640, epochs=400, workers=1, device=(0,1,2), verbose=True)
