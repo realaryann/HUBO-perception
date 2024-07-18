692401041e1ac37f59e877bbd400d6a46dc38398
MacksCohn <maxcohn@live.com>
2024-07-18 11:26:50 -0400

    Gpu needs batch size of multiple 3

--- image_parsing/image_parsing/datasets/train.py
+++ image_parsing/image_parsing/datasets/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8m.pt')
 
-model.train(data='./data.yml', batch=32, imgsz=640, epochs=400, workers=2, device=[0,1,2,3], verbose=True)
+model.train(data='./data.yml', batch=35, imgsz=640, epochs=400, workers=2, device=[0,1,2,3], verbose=True)
