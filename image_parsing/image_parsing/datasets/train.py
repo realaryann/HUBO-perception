645f3ecdc43d897a1a51f95591a22f5323cd7f64
MacksCohn <maxcohn@live.com>
2024-07-18 11:25:47 -0400

    Changed GPUS

--- image_parsing/image_parsing/datasets/train.py
+++ image_parsing/image_parsing/datasets/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8m.pt')
 
-model.train(data='./data.yml', batch=32, imgsz=640, epochs=400, workers=2, device=[4,5,6,7], verbose=True)
+model.train(data='./data.yml', batch=32, imgsz=640, epochs=400, workers=2, device=[0,1,2,3], verbose=True)
